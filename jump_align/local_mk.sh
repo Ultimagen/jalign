5f6292de7b186615540d3f2889432f5f341d5bab
Dror Kessler <dror27.kessler@gmail.com>
2025-01-14 13:33:40 +0200

    local build

---
+++ jump_align/local_mk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xv
+
+g++ -g -std=c++11 \
+ -I ../manta/manta-1.6.0/src/c++/lib/alignment/ \
+ -I ../manta/manta-1.6.0/src/c++/lib \
+ -I ../../homebrew/Cellar/boost/1.87.0/include \
+ \
+ ../manta/manta-1.6.0/src/c++/lib/blt_util/blt_exception.cpp \
+ ../manta/manta-1.6.0/src/c++/lib/blt_util/align_path.cpp \
+ ../manta/manta-1.6.0/src/c++/lib/blt_util/parse_util.cpp \
+ jump_align.cpp \
+ -o jump_align
+
