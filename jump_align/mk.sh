185b1f7671250e477b828a5631c02b251af60a70
Gavrie Philipson <gavrie@philipson.co.il>
2025-03-31 09:36:53 +0000

    Update makefile for dev container

--- jump_align/mk.sh
+++ jump_align/mk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -xv
 
 g++ -O3 -Werror \
- -I /manta/manta-1.6.0/src/c++/lib/alignment/ \
- -I /manta/manta-1.6.0/src/c++/lib \
+ -I ../manta/src/c++/lib/alignment/ \
+ -I ../manta/src/c++/lib \
  -DNDEBUG \
  \
- /manta/manta-1.6.0/src/c++/lib/blt_util/blt_exception.cpp \
- /manta/manta-1.6.0/src/c++/lib/blt_util/align_path.cpp \
- /manta/manta-1.6.0/src/c++/lib/blt_util/parse_util.cpp \
+ ../manta/src/c++/lib/blt_util/blt_exception.cpp \
+ ../manta/src/c++/lib/blt_util/align_path.cpp \
+ ../manta/src/c++/lib/blt_util/parse_util.cpp \
  jump_align.cpp \
  -o jump_align
 
