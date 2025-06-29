07523a8d8bffb4b527052672a8c062ccf918eb1a
Dror Kessler <dror27.kessler@gmail.com>
2025-06-29 18:33:03 +0200

    debug build shell script

---
+++ jump_align/debug_mk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+g++ -std=c++11 -g -Werror \
+ -Wno-#pragma-messages \
+ -I ../manta/manta-1.6.0/src/c++/lib/alignment/ \
+ -I ../manta/manta-1.6.0/src/c++/lib \
+ -I ../../homebrew/Cellar/boost/1.87.0/include \
+ \
+ ../manta/manta-1.6.0/src/c++/lib/blt_util/blt_exception.cpp \
+ ../manta/manta-1.6.0/src/c++/lib/blt_util/align_path.cpp \
+ ../manta/manta-1.6.0/src/c++/lib/blt_util/parse_util.cpp \
+ jump_align.cpp \
+ -o jump_align
+
